09239c40f76397b5d596e00e521c6c6eecb92978
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-01 12:43:32 -0800

    added test cases

--- index.test.js
+++ index.test.js
@@ -1,24 +1,34 @@
-const {sequelize} = require('./db');
-const {Band, Musician} = require('./index')
+const { sequelize } = require("./db");
+const { Band, Musician } = require("./index");
 
-describe('Band and Musician Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    })
+describe("Band and Musician Models", () => {
+  /**
+   * Runs the code prior to all tests
+   */
+  beforeAll(async () => {
+    // the 'sync' method will create tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the
+    // test suite is run
+    await sequelize.sync({ force: true });
+  });
 
-    test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test("can create a Band", async () => {
+    // TODO - test creating a band
+    const band = await Band.create({
+      name: "The Beatles",
+      genre: "Classic Rock",
+    });
+    expect(band.name).toBe("The Beatles");
+    expect(band.genre).toBe("Classic Rock");
+  });
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
-})
+  test("can create a Musician", async () => {
+    // TODO - test creating a band
+    const musician = await Musician.create({
+      name: "John Lennon",
+      instrument: "Guitar",
+    });
+    expect(musician.name).toBe("John Lennon");
+    expect(musician.genre).toBe("Guitar");
+  });
+});
