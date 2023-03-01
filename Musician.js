4bb0f6064c98713d5b1f739de0fae8cfe527b68a
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-01 12:53:14 -0800

    fixed bug

--- Musician.js
+++ Musician.js
@@ -1,7 +1,7 @@
 const { Sequelize, sequelize } = require("./db");
 
 // TODO - define the Musician model
-let Musician = Sequelize.define("musician", {
+let Musician = sequelize.define("musician", {
   name: Sequelize.STRING,
   instrument: Sequelize.STRING,
 });
