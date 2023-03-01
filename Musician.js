dfa73d9a02cb801edf9df1ec6afc324714ba3c90
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-01 12:31:33 -0800

    defined models

--- Musician.js
+++ Musician.js
@@ -1,8 +1,11 @@
-const {Sequelize, sequelize} = require('./db');
+const { Sequelize, sequelize } = require("./db");
 
 // TODO - define the Musician model
-let Musician;
+let Musician = Sequelize.define("musician", {
+  name: Sequelize.STRING,
+  instrument: Sequelize.STRING,
+});
 
 module.exports = {
-    Musician
+  Musician,
 };
